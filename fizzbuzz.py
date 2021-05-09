2d6cae38f33457f19f2e52a73e349bfb7482d8e1
Nils <nils@42nerds.com>
2021-05-09 22:03:17 +0000

    [IMP] refactoring

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -6,10 +6,9 @@ def fizz_buzz(start_number = 1, end_number = 50):
             fizz_buzz_value = "fizz"
         if number % 5 == 0:
             fizz_buzz_value += "buzz"
-        if fizz_buzz_value != "":
-            fizz_buzz_list.append(fizz_buzz_value)
-        else:
-            fizz_buzz_list.append(number)
+        if fizz_buzz_value == "":
+            fizz_buzz_value = number
+        fizz_buzz_list.append(fizz_buzz_value)
     return fizz_buzz_list
 
 
