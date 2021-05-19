1165dff3a14af3ecdf35de593a25bd5396b7a24b
Nils Schulze <nils@42nerds.com>
2021-05-19 13:29:15 +0200

    die Aufgabe

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,27 +1,46 @@
 #!/usr/bin/env python3
 
-def fizz_buzz(start_number = 1, end_number = 50):
+"""
+FizzBuzz
+
+Die Funktion gibt den Zahlraum von 1 bis 100 wieder.
+Bei jeder Zahl die durch drei teilbar ist, soll die Zahl durch "fizz" ersetzt werden.
+Bei jeder Zahl die durch fünf teilbar ist, soll die Zahl durch "buzz" ersetzt werden.
+Bei jeder Zahl die durch fünfzehn teilbar ist, soll die Zahl durch "fizzbuzz" ersetzt werden.
+"""
+
+def fizz_buzz(start_number, end_number):
     """
-        fizz_buzz is a function to solve the game fizz_buzz
-        Parameters
-        ----------
-        start_number : int, optional
-            the number to start in fizz_buzz (default is 1)
-        end_number : int, optional
-            the number to end in fizz_buzz (default is 50)
+    fizz_buzz is a function to solve the game fizz_buzz
+    Parameters
+    ----------
+    start_number : int, optional
+        the number to start in fizz_buzz (default is 1)
+    end_number : int, optional
+        the number to end in fizz_buzz (default is 50)
     """
-    fizz_buzz_list = []
-    for number in range(start_number, end_number + 1):
-        fizz_buzz_value = ""
-        if number % 3 == 0:
-            fizz_buzz_value = "fizz"
-        if number % 5 == 0:
-            fizz_buzz_value += "buzz"
-        if fizz_buzz_value == "":
-            fizz_buzz_value = number
-        fizz_buzz_list.append(fizz_buzz_value)
-    return fizz_buzz_list
+    print("Start von FizzBuzz")
+    # Hier kannst du einen Code schreiben.
+
+    print("Ende von FizzBuzz")
 
 
 if __name__ == '__main__':
     print(fizz_buzz())
+
+
+### Hilfestellung
+## So funktioniert die for-Schleife
+
+# for i in range(1, 100):
+#     print(i)
+
+
+## So funktioniert die if-Abfrage
+
+# if a == 2:
+#     print("a ist 2")
+# elif a == 7:
+#     print("a ist 7")
+# else:
+#     print("a ist etwas anderes")
